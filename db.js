433629c88f41a01ec6ec4f46d5768453b31bea21
Nafisniloy <77499825+Nafisniloy@users.noreply.github.com>
2022-10-16 11:10:36 +0600

    changing database from local to online

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose');
-const mongoURI='mongodb://localhost:27017/socialapp'
+const mongoURI='mongodb+srv://niloy:niloy123@cluster0.6k5pnzw.mongodb.net/social-paragon'
 const connectToMongo = ()=>{
  mongoose.connect(mongoURI,()=>{
     console.log('mongo server is now connected')
