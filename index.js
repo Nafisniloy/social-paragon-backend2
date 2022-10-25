ab15071d1d3bc75c5bf5a7776467414814ebb268
Nafisniloy <77499825+Nafisniloy@users.noreply.github.com>
2022-10-26 01:48:34 +0600

    Fixing error

--- index.js
+++ index.js
@@ -7,30 +7,41 @@ const app = express()
 var cors = require('cors')
 
 
-app.use(cors());
-app.options('*', cors());
+
 
 const port = process.env.PORT || 3530;
 app.use (express.json())
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
+// // Add headers before the routes are defined
+// app.use(function (req, res, next) {
+  
+  //   // Website you wish to allow to connect
+  //   res.setHeader('Access-Control-Allow-Origin', 'https://paragon.learnfacts.xyz');
+  
+  //   // Request methods you wish to allow
+  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+  //   // Request headers you wish to allow
+  //   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+  
+  //   // Set to true if you need the website to include cookies in the requests sent
+  //   // to the API (e.g. in case you use sessions)
+  //   // res.setHeader('Access-Control-Allow-Credentials', true);
+  
+  //   // Pass to next layer of middleware
+  //   next();
+  // });
+  app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+      next();
+  })
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://paragon.learnfacts.xyz');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+  // app.use(cors());
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 
 // available routes
