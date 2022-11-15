b1296781f60d4f8577346c25286e691ae4fffda3
Nafisniloy <77499825+Nafisniloy@users.noreply.github.com>
2022-11-15 21:37:48 +0600

    adding notification

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ connectToMongo();
 
 const express = require('express')
 const app = express()
-var cors = require('cors')
+var cors = require('cors');
+const fetchuser = require('./middleware/fetchuser');
 
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server,{cors:{origin:"*"}});
 
 
 
@@ -54,8 +58,37 @@ app.use('/api/messages',require('./routes/messages'))
 app.use('/api/posts',require('./routes/posts'))
 // app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`Social paragon backend listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Social paragon backend listening on port ${port}`)
+// })
+let ActiveUsers=[]
+server.listen(port, () => console.log(`Listening on port ${port}`));
+const addNewuser=(userId,socketId)=>{
+!ActiveUsers.some((user)=>user.userId===userId)&&
+ActiveUsers.push({userId,socketId})
+}
+const removeUser= (socketId)=>{
+  ActiveUsers= ActiveUsers.filter((user)=>user.socketId!== socketId)
+}
+const getUser=(userId)=>{
+  return ActiveUsers.find((user)=>user.userId===userId)
+}
+io.on('connection', function (socket) {
+  socket.on("sendingId",(userId)=>{
+    addNewuser(userId,socket.id)
+  })
+  socket.on("newMessage", function(userId){
+    let user=getUser(userId)
+    // console.log(user)
+    io.to(user.socketId).emit("receivedMessage")
+  })
+  socket.on("disconnect",()=>{
+    removeUser(socket.id)
+  })
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+});
+
 
 module.exports = app;
